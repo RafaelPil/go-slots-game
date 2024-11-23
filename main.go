0f724f33c43c1f3702334cea1837559452add5c9
RafaelPil <rafael.pilumyan@gmail.com>
2024-11-23 17:17:31 +0200

    Update: checkWin

--- main.go
+++ main.go
@@ -2,87 +2,27 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 )
 
-func getName() string {
-	name := ""
+func checkWin(spin [][]string, multipliers map[string]uint) []uint {
+	lines := []uint{}
 
-	fmt.Println("Welcome to Goats Casino...")
-	fmt.Printf("Enter your name: ")
-	_, err := fmt.Scanln(&name)
-	if err != nil {
-		return ""
-	}
-	fmt.Printf("Welcome %s, let's play!\n", name)
-	return name
-}
-
-func getBet(balance uint) uint {
-	var bet uint
-	for true {
-		fmt.Printf("Enter your bet, or 0 to quit (balance = $%d): ", balance)
-		fmt.Scan(&bet)
-
-		if bet > balance {
-			fmt.Println("Bet cannot be larger then balance.")
-		} else {
-			break
-		}
-	}
-	return bet
-}
-
-func generateSymbolArray(symbols map[string]uint) []string {
-	symbolArr := []string{}
-
-	for symbol, count := range symbols{
-		for i:= uint(0); i<count; i++ {
-			symbolArr = append(symbolArr, symbol)
-		}
-	}
-	return symbolArr
-}
-
-func getRandomNumber(min int, max int) int {
-	randomNumber := rand.Intn(max - min + 1) + min
-	return randomNumber
-}
-
-func getSpin(reel []string, rows int, cols int) [][]string {
-	result := [][]string {}
-
-	for i:=0; i<rows; i++ {
-		result = append(result, []string{})
-	}
-
-	for col:= 0; col < cols; col++ {
-		selected := map[int]bool{}
-		for row:=0; row<rows; row++ {
-			for true {
-				randomIndex := getRandomNumber(0, len(reel) - 1)
-				_, exists := selected[randomIndex]
-				if !exists {
-					selected[randomIndex] = true
-					result[row] = append(result[row], reel[randomIndex])
-					break
-				}
-			}
-		}
-	}
-	return result
-}
-
-func printSpin(spin [][]string) {
 	for _, row := range spin {
-		for j, symbol := range row {
-			fmt.Printf(symbol)
-			if j != len(row) - 1 {
-				fmt.Print(" | ")
+		win := true
+		checkSymbol := row[0]
+		for _, symbol := range row[1:] {
+			if checkSymbol != symbol {
+				win = false
+				break
 			}
 		}
-		fmt.Println()
+		if win {
+			lines = append(lines, multipliers[checkSymbol])
+		} else {
+			lines = append(lines, 0)
+		}
 	}
+	return lines
 }
 
 func main() {
@@ -92,27 +32,35 @@ func main() {
 		"C": 12,
 		"D": 20,
 	}
-	// multipliers := map[string]uint {
-	// 	"A": 20,
-	// 	"B": 10,
-	// 	"C": 5,
-	// 	"D": 2,
-	// }
+	multipliers := map[string]uint {
+		"A": 20,
+		"B": 10,
+		"C": 5,
+		"D": 2,
+	}
 
-	symbolArr := generateSymbolArray(symbols)
+	symbolArr := GenerateSymbolArray(symbols)
 
 	balance := uint(200)
-	getName()
+	GetName()
 
 	for balance > 0 {
-		bet := getBet(balance)
+		bet := GetBet(balance)
 		if bet == 0 {
 			break
 		}
 		balance -= bet
-		spin := getSpin(symbolArr, 3, 3)
-		printSpin(spin)
+		spin := GetSpin(symbolArr, 3, 3)
+		PrintSpin(spin)
 		// check win, update balance
+		winningLines := checkWin(spin, multipliers)
+		for i, multi := range winningLines {
+			win := multi * bet
+			balance += win
+			if multi > 0 {
+				fmt.Printf("won $%d, (%dx) on Line #%d\n", win, multi, i + 1)
+			}
+		}
 	}
 	fmt.Printf("You left with, $%d.\n", balance)
 }
